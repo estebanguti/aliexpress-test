0344084e91f36a9efe3f523996cb25b6f0c9efd2
Esteban Gutierrez <guti1230@hotmail.com>
2022-02-22 19:27:37 -0500

    Adding stepdefinitions, TestRunner and feature

---
+++ src/main/java/com/aliexpress/test/AliExpressTestApplication.java
@@ -0,0 +1,13 @@
+package com.aliexpress.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliExpressTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliExpressTestApplication.class, args);
+    }
+
+}
